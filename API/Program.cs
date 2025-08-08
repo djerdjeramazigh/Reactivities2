1e993ec410b13e051eb1fd2763da4dc196966fcb
djerdjeramazigh <djerdjeramazigh@gmail.com>
2025-08-08 21:02:01 +0200

    section1

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
